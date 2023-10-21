94c69d96da655dd1d0148abf27efd2891b92ccfd
Jenifer Placido <jenifer.sdti@gmail.com>
2023-10-21 15:45:22 -0300

    DEPLOY

--- src/main/java/com/example/crud/CrudAlunosApplication.java
+++ src/main/java/com/example/crud/CrudAlunosApplication.java
@@ -2,11 +2,12 @@ package com.example.crud;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API DE ALUNOS by: JENIFER PLÁCIDO", version = "1", description = "API desenvolvida para cadastro de alunos"))
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")}, info =@Info(title = "API DE ALUNOS by: JENIFER PLÁCIDO", version = "1", description = "API desenvolvida para cadastro de alunos"))
 public class CrudAlunosApplication {
 
     public static void main(String[] args) {
