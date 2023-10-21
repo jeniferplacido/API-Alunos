22a4158516cf0673cfc5e3c5dae3481e89932833
Jenifer Placido <jenifer.sdti@gmail.com>
2023-10-21 14:23:13 -0300

    DEPLOY

--- src/main/java/com/example/crud/config/CorsConfig.java
+++ src/main/java/com/example/crud/config/CorsConfig.java
@@ -1,20 +1,16 @@
 package com.example.crud.config;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:8080/swagger-ui/index.html#/");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
+
 }
